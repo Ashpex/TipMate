ddc7d446dc712c622830241439c25b2ff0757631
ashpex <ashpex@pm.me>
2021-02-09 03:33:29 +0700

    Add fuctional tip calculator

--- app/src/main/java/com/ashpex/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/ashpex/tipcalculator/MainActivity.kt
@@ -2,10 +2,33 @@ package com.ashpex.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ashpex.tipcalculator.databinding.ActivityMainBinding
+import java.lang.NumberFormatException
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{calculateTip()}
+    }
+    fun calculateTip(){
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when(selectedId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_fifteen_percent -> 0.15
+            else -> 0.10
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
