cd0a1bd3ee5118fa5cb75bacdbf37a070e3f6871
ashpex <ashpex@pm.me>
2021-02-10 03:32:26 +0700

    Enhance UI

--- app/src/main/java/com/ashpex/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/ashpex/tipcalculator/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ashpex.tipcalculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.ashpex.tipcalculator.databinding.ActivityMainBinding
-import java.lang.NumberFormatException
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost == null) {
             binding.tipResult.text = ""
@@ -34,4 +37,15 @@ class MainActivity : AppCompatActivity() {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
 }
